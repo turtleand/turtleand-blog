e0f79bb6ae40e8bac58c9c491bd334ca3fbc9831
Turtleand <turtliophant@gmail.com>
2026-06-22 23:50:54 -0300

    Fix mobile tap targets (#41)
    * Fix mobile tap targets
    
    * Address icon link tap target width
    
    ---------
    
    Co-authored-by: TurtleandHermes <hermes@turtleand.com>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,7 +20,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
     <li className="my-6">
       <a
         href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="inline-flex min-h-12 items-center text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
         {secHeading ? (
           <h2 {...headerProps}>{title}</h2>
